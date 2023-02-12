6e4b9bc5e3f97b48f07f007f71e18c7e709c915d
unknown <luukleijtens@gmail.com>
2023-02-12 17:56:18 +0100

    Update

--- snippets/button/index.js
+++ snippets/button/index.js
@@ -5,7 +5,26 @@ const snippet = {
   htmlVariants: {
     basic: `<button class="button" type="button">Button</button>`,
     disabled: `<button class="button" type="button" disabled>Button</button>`,
-    'custom color': `
+    sizes: `
+<style>
+.button-small {
+  --_padding-x: 0.5rem;
+  --_padding-y: 0.25rem;
+  --_font-size: 0.875rem;
+}
+
+.button-large {
+  --_padding-x: 1rem;
+  --_padding-y: 0.5rem;
+  --_font-size: 1.25rem;
+}
+</style>
+
+<button class="button button-small" type="button">Small button</button>
+<button class="button" type="button">Default button</button>
+<button class="button button-large" type="button">Large button</button>
+        `,
+    "custom color": `
 <style>
 .button-custom {
   --_color: #ffffff;
@@ -24,7 +43,7 @@ const snippet = {
 
 <button class="button button-custom" type="button">Button</button>
     `,
-    'custom variants':`
+    "custom variants": `
 <style>
 .button-outline-custom {
   --_color: #4263eb;
@@ -43,41 +62,22 @@ const snippet = {
 .button-subtle-custom {
   --_color: #4263eb;
   --_bg: #ffffff;
-  --_border-color: #4263eb;
+  --_border-color: #ffffff;
 
-  --_hover-color: #ffffff;
-  --_hover-bg: #3b5bdb;
-  --_hover-border-color: #3b5bdb;
+  --_hover-color: #4263eb;
+  --_hover-bg: #edf2ff;
+  --_hover-border-color: #edf2ff;
 
-  --_active-color: #ffffff;
-  --_active-bg: #364fc7;
-  --_active-border-color: #364fc7;
+  --_active-color: #4263eb;
+  --_active-bg: #dbe4ff;
+  --_active-border-color: #dbe4ff;
 }
 </style>
 
 <button class="button button-outline-custom" type="button">Button outline</button>
 <button class="button button-subtle-custom" type="button">Button subtle</button>
     `,
-    sizes:`
-<style>
-.button-small {
-	--_padding-x: 0.5rem;
-  --_padding-y: 0.25rem;
-	--_font-size: 0.875rem;
-}
-
-.button-large {
-	--_padding-x: 1rem;
-  --_padding-y: 0.5rem;
-	--_font-size: 1.25rem;
-}
-</style>
-
-<button class="button button-small" type="button">Small button</button>
-<button class="button" type="button">Default button</button>
-<button class="button button-large" type="button">Large button</button>
-    `,
-  }
-}
+  },
+};
 
 export default snippet;
