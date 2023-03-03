a83745aab9467198e16a355c9c8cda8fd05a2ca4
luuk-leijtens <luukleijtens@hotmail.com>
2023-03-03 08:47:53 +0100

    update

--- snippets/button/index.js
+++ snippets/button/index.js
@@ -1,5 +1,5 @@
 import styles from "./index.css?inline";
-import iconStyles from "../icon/index.css";
+import iconStyles from "../icon/index.css?inline";
 
 const $default = `<button class="button">Button</button>`;
 const disabled = `<button class="button" disabled>Button</button>`;
